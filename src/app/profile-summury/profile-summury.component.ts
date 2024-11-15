59407e32234ea6d9298109d4c36ebef3a359ca63
Simone Francesco Albino <simonefrancesco.albino@gmail.com>
2024-11-15 21:54:01 +0100

    - Automatic age calculation - Changed birthday format

--- src/app/profile-summury/profile-summury.component.ts
+++ src/app/profile-summury/profile-summury.component.ts
@@ -1,12 +1,25 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-summury',
   standalone: true,
   imports: [],
+  providers: [DatePipe],
   templateUrl: './profile-summury.component.html',
   styleUrl: './profile-summury.component.css'
 })
 export class ProfileSummuryComponent {
+  private bornDate: Date = new Date(2003, 11, 6);
 
+  constructor(private datePipe: DatePipe) {}
+
+  get birthday(): string {
+    return this.datePipe.transform(this.bornDate, 'd MMM y')!;
+  }
+
+  get age(): number {
+    const now = new Date();
+    return now.getFullYear() - this.bornDate.getFullYear();
+  }
 }
